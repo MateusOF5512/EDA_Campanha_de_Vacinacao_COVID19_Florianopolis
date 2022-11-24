082f0f7c0b997a40f0a9e4a26fd39a1cb30e4fea
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-11-24 13:06:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="App Web", page_icon=":mag_right:", layout="wide")
 
 hide_streamlit_style = """
             <style>
-            footer {visibility: hidden;}
+            #footer {visibility: hidden;}
             </style> """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
