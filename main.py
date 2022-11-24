d5d8cd28f3b3963003c6a4b429efd17a910575a1
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-11-24 13:08:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import streamlit as st
 
 st.set_page_config(page_title="App Web", page_icon=":mag_right:", layout="wide")
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style> 
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 from pages import *
 from plots import *
